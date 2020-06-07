6d898a1aa8be73d26934aa714810b5ae0ff4687f
Daniel A. Nagy <nagy.da@gmail.com>
2020-06-08 01:49:36 +0200

    Generators unified

--- generators.asm
+++ generators.asm
@@ -1,5 +1,30 @@
-seq:
+countdown:
 	pop	de
+	pop	hl
+
+; Countdown generator
+; N:counter
+N_countdown:
+;	vm
+;	defw	N_A_swap
+;	defw	N_decr
+;	defw	A_N_swap
+;; this is shorter and faster
+	dec	hl
+	ld	a, h
+	and	l
+	add	a, 1
+	vm
+;;
+	defw	if
+	defb	  countdown - $
+	defw	argN
+	defb	  8	; counter
+	defw	generate
+	defw	end
+
+
+seq:	pop	de
 	pop	hl
 	vm
 	defw	argN
@@ -24,9 +49,13 @@ N_N_N_seq:
 	defb	  seq - $
 	defw	argN
 	defb	  0xC	; from
-	defw	cpu
+	defw	generate
+	defw	end
+
+generate:
+	ex	de, hl
 	push	hl
-	ld	hl, 6
+	ld	hl, 8
 	add	hl, sp
 	ld	a, (hl)
 	inc	hl
