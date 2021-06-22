fd1a6e846de13746cad26cc2b75e3860fee28e7d
Daniel A. Nagy <nagy.da@gmail.com>
2021-06-22 08:41:16 +0200

    interpreter reports failure

--- threaded/interpreter.asm
+++ threaded/interpreter.asm
@@ -6,7 +6,16 @@ interpret:
 iword:		vm
 		defw	find
 		defw	cellplus
-		defw	tail, exec
+		defw	litN16, exec
+		defw	litS8
+		defb	  ifaile - ifail
+ifail:			vm
+			defw	litS8
+			defb	  ifailte - ifailt
+ifailt:				defb	" fail", 13, 0
+ifailte:		defw	type
+			defw	fail
+ifaile:		defw	tailor
 iworde:	defw	litS8
 	defb	  inume - inum
 inum:		vm
